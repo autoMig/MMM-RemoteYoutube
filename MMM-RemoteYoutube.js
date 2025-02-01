8d58110bb985226f79fe493b5e8326bfa03363ef
Chloe Altmann <chloe.altmann@iteratec.com>
2025-02-01 12:05:31 +0100

    Add: First version of MMM-RemoteYoutube

---
+++ MMM-RemoteYoutube.js
@@ -0,0 +1,266 @@
+Module.register("MMM-RemoteYoutube", {
+    defaults: {
+        debug: false,
+        width: "640",
+        height: "390",
+        playlistIds: [],
+        hideDelay: 2 * 60 * 1000,
+        volumeFactor: 1,
+        showControl: false,
+        defaultVolume: 50,
+        resetVolumeOnHide: false,
+        enableLoop: true,
+        enableShuffle: false,
+    },
+
+    playerIsReady: false,
+    currentVolume: 0,
+    hideTimer: null,
+    currentPlaylistIndex: 0,
+    playlistVideoIndices: {},
+
+    start: function () {
+        Log.info("Starting module: " + this.name)
+        if (this.config.playlistIds.length > 0) {
+            this.loadYouTubeIframeAPI()
+        } else {
+            Log.error(`[${this.name}] Can not start without at least one playlist.`)
+        }
+    },
+
+    getDom: function () {
+        const wrapper = document.createElement("div")
+        wrapper.id = "youtube-player"
+        return wrapper
+    },
+
+    notificationReceived: function (notification, payload) {
+        switch (notification) {
+            case "MMM_REMOTEYOUTUBE_PLAY":
+                this.playVideo()
+                break
+            case "MMM_REMOTEYOUTUBE_PAUSE":
+                this.pauseVideo()
+                break
+            case "MMM_REMOTEYOUTUBE_STOP":
+                this.stopPlayer()
+                break
+            case "MMM_REMOTEYOUTUBE_NEXT":
+                this.nextVideo()
+                break
+            case "MMM_REMOTEYOUTUBE_PREVIOUS":
+                this.previousVideo()
+                break
+            case "MMM_REMOTEYOUTUBE_SWITCH_PLAYLIST":
+                this.switchPlaylist()
+                break
+            case "MMM_REMOTEYOUTUBE_INCREASE_VOLUME":
+                this.increaseVolume(payload)
+                break
+            case "MMM_REMOTEYOUTUBE_DECREASE_VOLUME":
+                this.decreaseVolume(payload)
+                break
+        }
+    },
+
+    playVideo: function () {
+        if (this.hidden) {
+            clearTimeout(this.hideTimer)
+            this.show(1000)
+        }
+
+        if (this.playerIsReady) {
+            const playerState = this.player.getPlayerState()
+            if (playerState !== YT.PlayerState.PLAYING) {
+                this.player.playVideo()
+                if (this.config.debug) {
+                    Log.info(`[${this.name}] Playing video`)
+                }
+            } else {
+                this.player.pauseVideo()
+                if (this.config.debug) {
+                    Log.info(`[${this.name}] Pausing video (was playing)`)
+                }
+            }
+        }
+    },
+
+    pauseVideo: function () {
+        if (this.playerIsReady) {
+            const playerState = this.player.getPlayerState()
+            if (playerState === YT.PlayerState.PLAYING) {
+                this.player.pauseVideo()
+                if (this.config.debug) {
+                    Log.info(`[${this.name}] Pausing video`)
+                }
+            } else if (this.config.debug) {
+                Log.info(`[${this.name}] Video is already paused or not playing`)
+            }
+        }
+    },
+
+    stopPlayer: function () {
+        if (this.playerIsReady) {
+            this.player.stopVideo()
+            this.currentVolume = this.config.defaultVolume
+            this.player.setVolume(this.config.defaultVolume)
+            this.hide(1000)
+            if (this.config.debug) {
+                Log.info(`[${this.name}] Stop video and hide player`)
+            }
+        }
+    },
+
+    nextVideo: function () {
+        if (this.playerIsReady) {
+            this.player.nextVideo()
+            if (this.config.debug) {
+                Log.info(`[${this.name}] Skipping to next video`)
+            }
+        }
+    },
+
+    previousVideo: function () {
+        if (this.playerIsReady) {
+            this.player.previousVideo()
+            if (this.config.debug) {
+                Log.info(`[${this.name}] Skipping to previous video`)
+            }
+        }
+    },
+
+    switchPlaylist: function () {
+        if (this.playerIsReady) {
+            if (this.config.playlistIds.length === 0) {
+                if (this.config.debug) {
+                    Log.warn(`[${this.name}] No playlists configured`)
+                }
+                return
+            }
+
+            // Save the current video index for the current playlist
+            const currentPlaylistId = this.config.playlistIds[this.currentPlaylistIndex]
+            this.playlistVideoIndices[currentPlaylistId] = this.player.getPlaylistIndex()
+
+            const newPlaylistIndex = (this.currentPlaylistIndex + 1) % this.config.playlistIds.length
+            const newPlaylistId = this.config.playlistIds[newPlaylistIndex]
+
+            this.player.stopVideo()
+            const startIndex = this.playlistVideoIndices[newPlaylistId] || 0
+            this.player.loadPlaylist({
+                listType: "playlist",
+                list: newPlaylistId,
+                index: startIndex
+            })
+            this.player.setShuffle(this.config.enableShuffle)
+            this.currentPlaylistIndex = newPlaylistIndex
+            if (this.config.debug) {
+                Log.info(`[${this.name}] Switched to playlist: ${newPlaylistId}`)
+            }
+        }
+    },
+
+    increaseVolume: function (notificationPayload) {
+        if (this.playerIsReady) {
+            const increaseBy = this.convertPayloadToVolume(notificationPayload)
+            if (increaseBy === null) return
+
+            let newVolume = this.currentVolume + increaseBy
+            newVolume = Math.max(0, Math.min(100, newVolume))
+
+            this.player.setVolume(newVolume)
+            this.currentVolume = newVolume
+            if (this.config.debug) {
+                Log.info(`[${this.name}] Increased volume to: ${this.player.getVolume()}`)
+            }
+        }
+    },
+
+    decreaseVolume: function (notificationPayload) {
+        if (this.playerIsReady) {
+            const decreaseBy = this.convertPayloadToVolume(notificationPayload)
+            if (decreaseBy === null) return
+
+            let newVolume = this.currentVolume - decreaseBy
+            newVolume = Math.max(0, Math.min(100, newVolume))
+
+            this.player.setVolume(newVolume)
+            this.currentVolume = newVolume
+            if (this.config.debug) {
+                Log.info(`[${this.name}] Decreased volume to: ${this.player.getVolume()}`)
+            }
+        }
+    },
+
+    convertPayloadToVolume: function (notificationPayload) {
+        let payloadValue = null
+
+        if (typeof notificationPayload === "number") {
+            payloadValue = notificationPayload
+        } else if (typeof notificationPayload === "object" && notificationPayload !== null && "value" in notificationPayload) {
+            payloadValue = notificationPayload.value
+        } else if (this.config.debug) {
+            Log.warn("Unexpected notificationPayload format:", notificationPayload)
+            return null
+        }
+
+        // Adjust the payload value using the volume factor, if applicable
+        const adjustedValue = this.config.volumeFactor ? payloadValue / this.config.volumeFactor : payloadValue
+        return Math.round(adjustedValue)
+    },
+
+    loadYouTubeIframeAPI: function () {
+        var tag = document.createElement("script")
+        tag.src = "https://www.youtube.com/iframe_api"
+        var firstScriptTag = document.getElementsByTagName("script")[0]
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+        window.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady.bind(this)
+    },
+
+    onYouTubeIframeAPIReady: function () {
+        this.player = new YT.Player("youtube-player", {
+            width: this.config.width,
+            height: this.config.height,
+            playerVars: {
+                listType: "playlist",
+                list: this.config.playlistIds[0],
+                controls: this.config.showControl,
+            },
+            events: {
+                onReady: this.onPlayerReady.bind(this),
+                onStateChange: this.handleVideoStateChange.bind(this)
+            }
+        })
+    },
+
+    onPlayerReady: function () {
+        this.playerIsReady = true
+        this.player.setLoop(this.config.enableLoop)
+        this.player.setShuffle(this.config.enableShuffle)
+        this.player.setVolume(this.config.defaultVolume)
+        this.currentVolume = this.config.defaultVolume
+
+        if (this.config.debug) {
+            Log.info(`[${this.name}] YouTube player is ready`)
+        }
+    },
+
+    handleVideoStateChange: function (event) {
+        clearTimeout(this.hideTimer)
+
+        if (event.data === YT.PlayerState.PLAYING) {
+            const currentPlaylistId = this.config.playlistIds[this.currentPlaylistIndex]
+            this.playlistVideoIndices[currentPlaylistId] = this.player.getPlaylistIndex()
+        } else if (event.data === YT.PlayerState.PAUSED) {
+            if (this.config.hideDelay !== 0) {
+                this.hideTimer = setTimeout(() => {
+                    if (this.config.resetVolumeOnHide) {
+                        this.currentVolume = this.config.defaultVolume
+                        this.player.setVolume(this.config.defaultVolume)
+                    }
+                    this.hide(1000)
+                }, this.config.hideDelay)
+            }
+        }
+    }
+})
---
+++ README.md
@@ -0,0 +1,109 @@
+# MMM-RemoteYoutube
+
+This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/) that allows you to play YouTube videos and playlists using notifications.
+
+![Example of MMM-RemoteYoutube](./images/screen.png)
+
+The player uses the YouTube iFrame API. For more information, see [YouTube iFrame API Reference](https://developers.google.com/youtube/iframe_api_reference).
+
+### Features
+
+- Play YouTube videos from given playlists
+- Control playback (play, pause, stop, next, previous)
+- Switch between multiple playlists
+- Adjust volume
+- Auto-hide when paused (configurable)
+- Shuffle and loop options
+
+## Installation
+
+In your terminal, go to your MagicMirror² Module folder, clone MMM-RemoteYoutube, and install its dependencies:
+
+   ```bash
+   cd ~/MagicMirror/modules
+   git clone https://github.com/DreamyChloe/MMM-RemoteYoutube.git
+   cd MMM-RemoteYoutube
+   npm install
+   ```
+
+### Update
+
+To update the module, navigate to the module's folder, pull the latest changes, and reinstall dependencies:
+
+```bash
+cd ~/MagicMirror/modules/MMM-RemoteYoutube
+git pull
+npm install
+```
+
+## Using the module
+
+To use this module, add it to the modules array in the `config/config.js` file:
+
+```js
+{
+    module: 'MMM-RemoteYoutube',
+    position: 'bottom_right',
+    config: {
+        debug: true,
+        width: "640",
+        height: "390",
+        playlistIds: ["YOUR_PLAYLIST_ID_1", "YOUR_PLAYLIST_ID_2"],
+        hideDelay: 120000,  // 2 minutes
+        volumeFactor: 1,
+        showControl: false,
+        defaultVolume: 50,
+        resetVolumeOnHide: true,
+        enableLoop: true,
+        enableShuffle: false
+    }
+}
+```
+
+### Configuration Options
+
+| Option              | Required | Default | Description                                                                             |
+|---------------------|----------|---------|-----------------------------------------------------------------------------------------|
+| `debug`             | No       | false   | Enable debug logging                                                                    |
+| `width`             | No       | "640"   | Width of the YouTube player                                                             |
+| `height`            | No       | "390"   | Height of the YouTube player                                                            |
+| `playlistIds`       | Yes      | []      | Array of YouTube playlist IDs to play                                                   |
+| `hideDelay`         | No       | 120000  | Time in milliseconds to wait before hiding the player when paused (set to 0 to disable) |
+| `volumeFactor`      | No       | 1       | Factor to adjust volume changes (default: 1)                                            |
+| `showControl`       | No       | false   | Whether to show YouTube player controls                                                 |
+| `defaultVolume`     | No       | 50      | Default volume level (0-100)                                                            |
+| `resetVolumeOnHide` | No       | false   | Reset the current volume to default volume upon hide (disabled if hideDelay is 0)       |
+| `enableLoop`        | No       | true    | Whether to loop playlists                                                               |
+| `enableShuffle`     | No       | false   | Whether to shuffle playlists                                                            |
+
+### What 'volumeFactor' is for
+
+I created this module with a Hue Tap Dial as the controller in mind. The value range that can be emitted from the API is much higher (~15-600) than the accepted value range of the YouTube iFrame API (0-100). For example, by using a factor of 5, we can decrease the value per step from 15 to 3.
+
+## Notifications
+
+This module responds to the following notifications:
+
+| Notification                        | Description                                                             |
+|-------------------------------------|-------------------------------------------------------------------------|
+| `MMM_REMOTEYOUTUBE_PLAY`            | Play or pause the video                                                 |
+| `MMM_REMOTEYOUTUBE_PAUSE`           | Pause the video                                                         |
+| `MMM_REMOTEYOUTUBE_PAUSE`           | Stop the video, reset volume to default and hide the module immediately |
+| `MMM_REMOTEYOUTUBE_NEXT`            | Skip to the next video                                                  |
+| `MMM_REMOTEYOUTUBE_PREVIOUS`        | Go back to the previous video                                           |
+| `MMM_REMOTEYOUTUBE_SWITCH_PLAYLIST` | Switch to the next playlist                                             |
+| `MMM_REMOTEYOUTUBE_INCREASE_VOLUME` | Increase the volume (payload: number or object with 'value' property)   |
+| `MMM_REMOTEYOUTUBE_DECREASE_VOLUME` | Decrease the volume (payload: number or object with 'value' property)   |
+
+## Why does it show "This video is unavailable"?
+The YouTube API can't access playlists that are private. Make sure that the desired playlist is either public or unlisted.
+
+## Contributing
+
+If you find any bugs or have suggestions for improvements, please open an issue on the [GitHub repository](https://github.com/DreamyChloe/MMM-HueControl).
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE.md) file for details.
+
+[mm]: https://github.com/MagicMirrorOrg/MagicMirror
